11081855996dff16513323f2f64e77d1bddc94a6
Merry Cimakasky <mkcimakasky@gmail.com>
2019-04-18 13:20:33 -0700

    binary search

---
+++ src/test/java/BinarySearchTest.java
@@ -0,0 +1,25 @@
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class BinarySearchTest {
+
+    @Test
+    public void binarySearchTest() {
+        //happy path
+
+        int[] happyPath = {
+                1, 3, 4, 5, 6, 7
+        };
+
+        int key = 7;
+
+        assertTrue(true);
+
+
+    }
+
+
+}
