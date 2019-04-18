5dc7e3e23f6d8f45557a34276b4fcf5dd9f13c3b
Merry Cimakasky <mkcimakasky@gmail.com>
2019-04-18 13:25:00 -0700

    turn in

--- src/test/java/BinarySearchTest.java
+++ src/test/java/BinarySearchTest.java
@@ -10,12 +10,6 @@ public class BinarySearchTest {
     public void binarySearchTest() {
         //happy path
 
-        int[] happyPath = {
-                1, 3, 4, 5, 6, 7
-        };
-
-        int key = 7;
-
         assertTrue(true);
 
 
