9047fdd960d9560eee892298dfaa42a9f94304e0
Merry Cimakasky <mkcimakasky@gmail.com>
2019-04-20 16:57:38 -0700

    tests done

--- src/main/java/BinarySearch.java
+++ src/main/java/BinarySearch.java
@@ -2,11 +2,6 @@ public class BinarySearch {
 
     public static void main(String[] args) {
 
-        int[] testArray = {
-                1, 3, 4, 5, 6, 7
-        };
-
-        System.out.println(binarySearch(testArray, 7));
 
     }
 
--- src/test/java/BinarySearchTest.java
+++ src/test/java/BinarySearchTest.java
@@ -1,3 +1,4 @@
+import static java.util.Arrays.binarySearch;
 import static org.junit.Assert.*;
 
 import org.junit.Assert;
@@ -9,10 +10,25 @@ public class BinarySearchTest {
     @Test
     public void binarySearchTest() {
         //happy path
+        int[] testArray = {1, 3, 4, 5, 6, 7};
 
-        assertTrue(true);
+        int returnValue = (binarySearch(testArray, 7));
 
+        assertEquals(5, returnValue);
 
+        //empty array
+
+        int[] emptyArray = {};
+
+        int emptyReturnValue = (binarySearch(emptyArray, 0));
+
+        assertEquals(-1, emptyReturnValue);
+
+        int[] nonSequentialArray = {22, 5, 13, 4, 9, 0};
+
+        int nonSequentialReturn = (binarySearch(nonSequentialArray, 13));
+
+        assertEquals(2, nonSequentialReturn);
     }
 
 
